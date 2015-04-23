67bd866a30835f6d3e320a2f764962a4cd750bb4
ehab93 <ehalbadawy93@gmail.com>
2015-04-23 11:24:39 +0200

    Create a simple oauth test

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,10 +1,23 @@
-	window.onload = function() {
-		$('.single-item').slick({
-			dots: true,
-			infinite: true,
-			speed: 500,
-			fade: true,
-			cssEase: 'linear',
-			arrows: false
-		});
-	}
+window.onload = function() {
+	$('.single-item').slick({
+		dots: true,
+		infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'linear',
+		arrows: false
+	});
+ OAuth.initialize('qo-7OvDrW7SRNV0p6F1yoXfq33Q');
+
+	$('.login').click(function() {
+		OAuth.popup('github')
+	    .done(function(result) {
+	     	console.log(result);
+	     	console.log(result.access_token);
+	    })
+	    .fail(function (err) {
+	    	console.log(err);
+		});	
+	});	
+
+}
