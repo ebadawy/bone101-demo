b40a1ad693c08f8fcde6e2345670e2866e895a22
ehab93 <ehalbadawy93@gmail.com>
2015-04-24 10:26:30 +0300

    Get sha of img.txt

--- javascripts/script.js
+++ javascripts/script.js
@@ -9,6 +9,8 @@ window.onload = function() {
 	});
  	
  	var image;
+	var request_url = 'https://api.github.com/repos/ehab93/bone101-demo/contents/img.txt';
+
  	$('#inputFileToLoad').change(function() {
 		var filesSelected = document.getElementById("inputFileToLoad").files;
 		if (filesSelected.length > 0) {
@@ -18,17 +20,26 @@ window.onload = function() {
 				image = fileLoadedEvent.target.result;
 				console.log(image);
 			};
+			fileReader.readAsDataURL(fileToLoad);
 		}
 	});
 
  	OAuth.initialize('5mlO7j_7djd5808nB18KWjkE_ok');
-	$('.login').click(function() {
-		OAuth.popup('github')
-	    .done(function(result) {
-	     	console.log(result.access_token);
-	    })
-	    .fail(function (err) {
-	    	console.log(err);
+	$('.upload').click(function() {
+		OAuth.popup('github', function(err, rslt) {
+		    var access_token = rslt.access_token;
+		    rslt.me().done(function(me) {
+		        user = me.name;                
+		        username = me.alias; 
+		        email = me.email;
+		        $.ajax({
+		        	type: 'GET',
+		        	url: request_url,
+		        	success: function(data) {
+		        		console.log("SHA: " + data.sha);
+		        	}
+		        });
+		    })
 		});
-	});	
+	});
 }
