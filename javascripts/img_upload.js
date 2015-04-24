d6dca52a7d4791961676ec21c2b0a5b6d7daf6d4
ehab93 <ehalbadawy93@gmail.com>
2015-04-24 23:09:59 +0300

    Upload photos in user's gist instead of the repo

--- javascripts/img_upload.js
+++ javascripts/img_upload.js
@@ -1,6 +1,9 @@
 window.onload = function() {
  	var image;
+ 	var img_name;
 	var request_url = 'https://api.github.com/repos/ehab93/bone101-demo/contents/img.txt';
+    var access_token;
+    var gists = [];
 
  	$('#inputFileToLoad').change(function() {
 		var filesSelected = document.getElementById("inputFileToLoad").files;
@@ -9,73 +12,105 @@ window.onload = function() {
 			var fileReader = new FileReader();
 			fileReader.onload = function(fileLoadedEvent) {
 				image = fileLoadedEvent.target.result;
+				img_name = filesSelected[0].name;
 			};
 			fileReader.readAsDataURL(fileToLoad);
 		}
 	});
 
+ 	//if the user didn't login hide upload&refresh buttons
+	if(!access_token) {
+		$('.upload').hide();
+		$('.refresh').hide();
+	}
+
  	OAuth.initialize('5mlO7j_7djd5808nB18KWjkE_ok');
-	$('.upload').click(function() {
+	$('.login').click(function() {
 		OAuth.popup('github', function(err, rslt) {
-		    var access_token = rslt.access_token;
-		    rslt.me().done(function(me) {
-		        user = me.name;                
+		    $('.status').html('Please wait...');
+		    access_token = rslt.access_token;
+		    rslt.me().done(function(me) {    
+		    	$('.status').html('');
+    		    user = me.name;                
 		        username = me.alias; 
 		        email = me.email;
-		        $.ajax({
-		        	type: 'GET',
-		        	url: request_url + '?ref=gh-pages',
-		        	success: function(data) {
-		        		console.log('uploading...');
-		        		$('.status').html('uploading...');
-		        		params = {
-							message: "Update the image through github API",
-							committer: {
-								name: me.name,
-								email: me.email
-							},
-							content: btoa(image),
-							branch: 'gh-pages',
-							sha: data.sha
-						};
-
-						upload_file_request = {
-							url: request_url,
-							type: "PUT",
-							data: JSON.stringify(params)
-						}
-						upload_file_request.headers = {
-							"Authorization": 'token ' + access_token
-						}
-						upload_file_request.success = function(data) {
-		        			$('.status').html('The img is now uploaded.');
-							console.log('Uploaded!');
-						};
-						upload_file_request.error = function(err) {
-		        			$('.status').html('something went wrog! please check the console log.');
-							console.log(err);
-						}
-						$.ajax(upload_file_request);
-		        	}
-		        });
+		        $('.login').hide();
+		        $('.upload').show();
+				$('.refresh').show();
 		    })
 		});
 	});
 
+	$('.upload').click(function() {
+		if(img_name) {
+			file_name = 'encoded_' + img_name + '.txt';
+			var gist_params = {
+				description: 'upload '+ img_name + ' through bone101-demo',
+				public: true
+
+			}
+			gist_params['files']= {};
+			gist_params['files']['encoded_' + img_name + '.txt'] = {};
+			gist_params['files']['encoded_' + img_name + '.txt']['content'] = image;
+			var create_gist_request = {
+				type: 'POST',
+				url: 'https://api.github.com/gists',
+				data: JSON.stringify(gist_params),
+			}
+
+			create_gist_request.headers = {
+				"Authorization": 'token ' + access_token
+			}
+
+			create_gist_request.success = function(data) {
+				$('.status').html('The img is now uploaded.');
+				console.log('Uploaded!');
+			};
+			create_gist_request.error = function(err) {
+				$('.status').html('something went wrog! please check the console log.');
+				console.log(err);
+			}
+
+			$.ajax(create_gist_request);
+		} else
+			$('.status').html('please select a file.');
+	});
+
 	$('.refresh').click(function() {
 		    $('.status').html('Loading...');
+			$('.gists').empty();	
+
 			$.ajax({
 			type: 'GET',
-			url: request_url + '?ref=gh-pages',
+			url: 'https://api.github.com/users/' + username +'/gists',
 			success: function(data) {
-				$('.img-tag').attr('src', atob(data.content));	
-		    	$('.status').html('Done!');
+				var i = 0
+				gists = [];
+				$.each(data, function(index, val) {
+					if(Object.keys(val.files)[0].indexOf('encoded_') > -1)
+						gists.push(val);
+				});
+				.log(gists);
+				$.each(gists, function(index, gist) {
+					$.ajax({
+						type: 'GET',
+						url: 'https://api.github.com/gists/' + gist.id,
+						success: function(response) {
+		    				$('.status').html('Done!');
+							current_file_name = Object.keys(gist.files)[0];
+							$('.gists').append('<img src="' + response.files[current_file_name].content+'">');
+						},
+						error: function(error) {
+							console.log(error);
+    						$('.status').html('something went wrog! please check the console log.');
+						}
+					});
+				})
 			},
 			error: function(err) {
 				console.log(err);
     			$('.status').html('something went wrog! please check the console log.');
 			}
 		});
-		
 	});
 }
